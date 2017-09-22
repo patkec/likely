686328dde160df24870170cce10f63909af5bcfb
Simon Stamcar <patkec@users.noreply.github.com>
2017-09-22 20:16:06 +0200

    Response cleanup

--- api/responses/unauthorized.js
+++ api/responses/unauthorized.js
@@ -9,27 +9,22 @@
  * e.g.:
  * ```
  * return res.unauthorized(
- *   'Unauthorized for liking a user.',
- *   'user/1/like'
+ *   'Unauthorized for liking a user.'
  * );
  * ```
  */
 
-module.exports = function unauthorized(data, options) {
+module.exports = function unauthorized(data) {
 
   // Get access to `req`, `res`, & `sails`
   var req = this.req;
   var res = this.res;
   var sails = req._sails;
 
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
   // Set status code
   res.status(401);
   // Set a challenge
-  res.set('WWW-Authenticate', options.scheme || 'Bearer');
+  res.set('WWW-Authenticate', 'Bearer');
 
   // Log error to console
   if (data !== undefined) {
@@ -46,33 +41,19 @@ module.exports = function unauthorized(data, options) {
 
   // If the user-agent wants JSON, always respond with JSON
   // If views are disabled, revert to json
-  if (req.wantsJSON || sails.config.hooks.views === false) {
-    return res.jsonx(data);
+  if (req.wantsJSON) {
+    return res.json(data);
   }
 
-  // Attempt to prettify data for views, if it's a non-error object
-  var viewData = data;
-  if (!(viewData instanceof Error) && 'object' == typeof viewData) {
-    try {
-      viewData = require('util').inspect(data, {depth: null});
+  // Render 401 view
+  res.render('401', data, function(err) {
+    // If the view doesn't exist, or an error occured, send json
+    if (err) {
+      return res.json(data);
     }
-    catch(e) {
-      viewData = undefined;
-    }
-  }
 
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: viewData, title: 'Unauthorized' });
-  }
-
-  // If no second argument provided, try to serve the implied view,
-  // but fall back to sending JSON(P) if no view can be inferred.
-  else return res.guessView({ data: viewData, title: 'Unauthorized' }, function couldNotGuessView () {
-    return res.jsonx(data);
+    // Otherwise, serve the `views/mySpecialView.*` page
+    res.render('401');
   });
-
 };
 
